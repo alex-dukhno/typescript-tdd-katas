d518b21aed14b5e7509c05f71007686ebf8d750f
Alex-Diez <alex.dukhno@icloud.com>
2017-12-26 18:18:46 +0200

    linked deque kata fourteenth day

---
+++ ts-katas/double-linked-deque-kata/day_14/__tests__/kata.test.ts
@@ -0,0 +1,55 @@
+import {LinkedDeque} from '../kata';
+
+describe('linked queue', () => {
+    let deque: LinkedDeque;
+
+    beforeEach(() => {
+        deque = new LinkedDeque();
+    });
+
+    test('push many front pop all front', () => {
+        deque.pushFront(1);
+        deque.pushFront(2);
+        deque.pushFront(3);
+
+        expect(deque.popFront()).toBe(3);
+        expect(deque.popFront()).toBe(2);
+        expect(deque.popFront()).toBe(1);
+        expect(deque.popFront()).toBe(undefined);
+    });
+
+    test('push many back pop all front', () => {
+        deque.pushBack(1);
+        deque.pushBack(2);
+        deque.pushBack(3);
+
+        expect(deque.popFront()).toBe(1);
+        expect(deque.popFront()).toBe(2);
+        expect(deque.popFront()).toBe(3);
+        expect(deque.popFront()).toBe(undefined);
+        expect(deque.popBack()).toBe(undefined);
+    });
+
+    test('push many back pop all back', () => {
+        deque.pushBack(1);
+        deque.pushBack(2);
+        deque.pushBack(3);
+
+        expect(deque.popBack()).toBe(3);
+        expect(deque.popBack()).toBe(2);
+        expect(deque.popBack()).toBe(1);
+        expect(deque.popBack()).toBe(undefined);
+        expect(deque.popFront()).toBe(undefined);
+    });
+
+    test('push many front pop all back', () => {
+        deque.pushFront(1);
+        deque.pushFront(2);
+        deque.pushFront(3);
+
+        expect(deque.popBack()).toBe(1);
+        expect(deque.popBack()).toBe(2);
+        expect(deque.popBack()).toBe(3);
+        expect(deque.popBack()).toBe(undefined);
+    });
+});
---
+++ ts-katas/double-linked-deque-kata/day_14/kata.ts
@@ -0,0 +1,64 @@
+class LinkedDeque {
+    private head: Node;
+    private tail: Node;
+
+    popFront() {
+        if (this.head == undefined) {
+            return undefined;
+        } else {
+            const item = this.head.item;
+            this.head = this.head.next;
+            if (this.head == undefined) {
+                this.tail = undefined;
+            }
+            return item;
+        }
+    }
+
+    popBack() {
+        if (this.tail == undefined) {
+            return undefined;
+        } else {
+            const item = this.tail.item;
+            this.tail = this.tail.prev;
+            if (this.tail == undefined) {
+                this.head = undefined;
+            }
+            return item;
+        }
+    }
+
+    pushFront(item: number) {
+        const node = new Node(item, this.head);
+        if (this.head == undefined) {
+            this.tail = node;
+        } else {
+            this.head.prev = node;
+        }
+        this.head = node;
+    }
+
+    pushBack(item: number) {
+        const node = new Node(item, undefined, this.tail);
+        if (this.tail == undefined) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+        this.tail = node;
+    }
+}
+
+class Node {
+    item: number;
+    next: Node;
+    prev: Node;
+
+    constructor(item: number, next: Node = undefined, prev: Node = undefined) {
+        this.item = item;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+export {LinkedDeque};
