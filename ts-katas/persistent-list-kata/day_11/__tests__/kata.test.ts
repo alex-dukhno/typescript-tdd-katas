0f7355e1bfca68608d97b35438d3e31cd8fc3e0a
Alex-Diez <alex.dukhno@icloud.com>
2018-01-08 21:05:30 +0200

    persistent list kata eleventh day

---
+++ ts-katas/persistent-list-kata/day_11/__tests__/kata.test.ts
@@ -0,0 +1,53 @@
+import {List} from '../kata';
+
+describe('persistent list', () => {
+    const empty = List.empty();
+
+    test('create empty list', () => {
+        expect(empty.toString()).toBe('[]');
+    });
+
+    test('append items to list', () => {
+        const list = empty.append(1).append(2).append(3);
+
+        expect(list.toString()).toBe('[3, 2, 1]');
+    });
+
+    test('head of empty list', () => {
+        expect(empty.head).toThrow(Error);
+    });
+
+    test('head of nonempty list', () => {
+        const list = empty.append(1).append(2).append(3);
+
+        expect(list.head()).toBe(3);
+    });
+
+    test('tail of empty list', () => {
+        expect(empty.tail).toThrow(Error);
+    });
+
+    test('tail of nonempty list', () => {
+        const list = empty.append(1).append(2).append(3);
+
+        expect(list.tail()).toEqual(empty.append(1).append(2));
+    });
+
+    test('concatenation of two empty lists', () => {
+        expect(empty.concat(empty)).toEqual(empty);
+    });
+
+    test('concatenation of empty and nonempty lists', () => {
+        const nonempty = empty.append(1).append(2).append(3);
+
+        expect(empty.concat(nonempty)).toEqual(nonempty);
+        expect(nonempty.concat(empty)).toEqual(nonempty);
+    });
+
+    test('concatenation of two nonempty lists', () => {
+        const nonempty = empty.append(1).append(2).append(3);
+        const theOther = empty.append(4).append(5).append(6);
+
+        expect(theOther.concat(nonempty)).toEqual(empty.append(1).append(2).append(3).append(4).append(5).append(6));
+    });
+});
